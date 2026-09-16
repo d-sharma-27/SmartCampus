e9d5f06649f5755520d398a96c86fe62d13f855f
d-sharma-27 <dharinisharma27082007@gmail.com>
2026-09-17 01:19:50 +0530

    Create AppConfig.java

---
+++ src/main/java/campus/util/AppConfig.java
@@ -0,0 +1,8 @@
+package campus.util;
+public class AppConfig {
+    public static final String APPLICATION_NAME =
+            "Smart Campus Resource Manager";
+    public static final String VERSION = "1.0";
+    private AppConfig() {
+    }
+}
