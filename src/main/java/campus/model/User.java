c11d2cee715621e96848c3686ce13db36db04a94
d-sharma-27 <dharinisharma27082007@gmail.com>
2026-09-17 00:48:04 +0530

    Create User.java

---
+++ src/main/java/campus/model/User.java
@@ -0,0 +1,53 @@
+package campus.model;
+import campus.enums.UserRole;
+public class User {
+    private int userId;
+    private String name;
+    private String email;
+    private String password;
+    private UserRole role;
+    public User(int userId, String name, String email,
+                String password, UserRole role) {
+        this.userId = userId;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+    public int getUserId() {
+        return userId;
+    }
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public UserRole getRole() {
+        return role;
+    }
+    public void setRole(UserRole role) {
+        this.role = role;
+    }
+    public void displayUserInfo() {
+        System.out.println("User ID: " + userId);
+        System.out.println("Name: " + name);
+        System.out.println("Email: " + email);
+        System.out.println("Role: " + role);
+    }
+}
